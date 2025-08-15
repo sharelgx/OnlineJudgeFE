fecb00079458a62bd73671b543e0db451943e92e
sharelgx <35207129+sharelgx@users.noreply.github.com>
2025-08-15 22:21:09 +0800

    添加组件导出配置，包含PDF导入和选择题组件

---
+++ src/pages/admin/views/index.js
@@ -0,0 +1,25 @@
+import Dashboard from './general/Dashboard.vue'
+import Announcement from './general/Announcement.vue'
+import User from './general/User.vue'
+import Conf from './general/Conf.vue'
+import JudgeServer from './general/JudgeServer.vue'
+import PruneTestCase from './general/PruneTestCase.vue'
+import Problem from './problem/Problem.vue'
+import ProblemList from './problem/ProblemList.vue'
+import ChoiceProblem from './choice/ChoiceProblem.vue'
+import ChoiceProblemList from './choice/ChoiceProblemList.vue'
+import ChoiceStatistics from './choice/ChoiceStatistics.vue'
+import PDFImport from './pdf/PDFImport.vue'
+import PDFImportDetail from './pdf/PDFImportDetail.vue'
+
+import ContestList from './contest/ContestList.vue'
+import Contest from './contest/Contest.vue'
+import Login from './general/Login.vue'
+import Home from './Home.vue'
+import ProblemImportOrExport from './problem/ImportAndExport.vue'
+
+
+export {
+  Announcement, User, Conf, JudgeServer, Problem, ProblemList, ChoiceProblem, ChoiceProblemList, ChoiceStatistics, Contest,
+  ContestList, Login, Home, PruneTestCase, Dashboard, ProblemImportOrExport, PDFImport, PDFImportDetail
+}
